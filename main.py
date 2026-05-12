d4b345ab394b8e7baa9f3a85887d234836cc1590
mabdelbaky2010 <mahmoud1.gamal@yahoo.com>
2026-05-12 17:59:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ CHAT_ID        = "1621604072"
 def send_telegram(message):
     url = f"https://api.telegram.org/bot8784816733:AAF2FpH9EqJ85BzVUjSXH1UI4McDIhSbNvI/sendMessage"
     data = {
-        "chat_id": CHAT_ID,
+        "CHAT_ID": CHAT_ID,
         "text": message,
         "parse_mode": "HTML"
     }
